75f6595cb6478ad4b7052cf2369d98447bfca313
Naveenkumar Gone <naveenkumargone1234@gmail.com>
2024-09-17 11:45:57 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+// server.js
+const express = require('express');
+const todoRoutes = require("./todoRoutes");
+const cors = require('cors');
+
+const server = express();
+
+// Middleware to parse JSON bodies
+server.use(express.json());
+
+// Enable CORS for development
+server.use(cors());
+
+// Use your routes
+server.use("/api/todos", todoRoutes.router);
+
+// A simple route for testing
+server.get("/", (req, res) => {
+    res.send("Welcome to Utility API");
+});
+
+// Start the server after all middleware and routes are set up
+const PORT = 4100;
+server.listen(PORT, () => {
+    console.log(`Server is listening at ${PORT}`);
+});
